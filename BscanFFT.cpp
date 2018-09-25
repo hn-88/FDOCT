2fcf5d32926d6f3cb8a93a91d95ce6a34236005f
hn-88 <hn-88@users.noreply.github.com>
2018-09-25 09:49:20 +0530

    linearized k, linear interpolation

--- BscanFFT.cpp
+++ BscanFFT.cpp
@@ -33,8 +33,8 @@
  * 
  * 
  * Hari Nandakumar
- * 15 Sep 2018
- *  
+ * 15 Sep 2018 to
+ * 25 Sep 2018
  * 			
  *			 
  * 			 
@@ -66,10 +66,6 @@
 // #include <opencv/highgui.h>
  
 
-
-
-// Interpolation using 
-
 using namespace cv;
 
 int main(int argc,char *argv[])
@@ -83,11 +79,13 @@ int main(int argc,char *argv[])
     unsigned int w,h,bpp=8,channels, cambitdepth=16, numofframes=100; 
     unsigned int numofm1slices=10, numofm2slices=10, firstaccum, secondaccum;
     unsigned int offsetx=0, offsety=0;
-    unsigned int indexi, averages=1, indextemp, opw, oph;
+    unsigned int indexi, averages=1, opw, oph;
+    int  indextemp, indextempl;
 
      
     int camtime = 1,camgain = 1,camspeed = 1,cambinx = 2,cambiny = 2,usbtraffic = 10;
     int camgamma = 1, binvalue=1, normfactor=1, normfactorforsave=25;
+    int numfftpoints=1024;
     
      
     bool doneflag=0, skeypressed=0, bkeypressed=0;
@@ -109,6 +107,13 @@ int main(int argc,char *argv[])
 	namedWindow("Bscan",0); // 0 = WINDOW_NORMAL
 	moveWindow("Bscan", 800, 0);
 	
+	
+	namedWindow("linearized",0); // 0 = WINDOW_NORMAL
+	moveWindow("linearized", 20, 500);
+	
+	
+	namedWindow("Bscanl",0); // 0 = WINDOW_NORMAL
+	moveWindow("Bscanl", 400, 0);
 	
 	char dirname[80];
 	char filename[20];
@@ -148,6 +153,8 @@ int main(int argc,char *argv[])
 			infile >> dirdescr;
 			infile >> tempstring;
 			infile >> averages;
+			infile >> tempstring;
+			infile >> numfftpoints;
 			infile.close();
 		  }
 
@@ -156,8 +163,8 @@ int main(int argc,char *argv[])
 			
 	   
 	   
-    ///////////////////////
-    // init camera etc
+    /////////////////////////////////////
+    // init camera, variables, etc
     
     cambitdepth = bpp;
     opw = w/binvalue;
@@ -167,15 +174,22 @@ int main(int argc,char *argv[])
 	Mat plot_result;
 	Mat plot_result2;
 	 
-	Mat data_y( opw, oph, CV_64F );
-	Mat data_yb( opw, oph, CV_64F );
-	Mat padded;
+	Mat data_y( oph, opw, CV_64F );		// the Mat constructor Mat(rows,columns,type)
+	Mat data_ylin( oph, numfftpoints, CV_64F );
+	Mat data_yb( oph, opw, CV_64F );
+	Mat padded, paddedn;
+	
+	// initialize data_yb with zeros
+	data_yb = Mat::zeros(Size(opw, oph), CV_64F);		//Size(cols,rows)		
 	 
 	int nr, nc;
 	
 	Mat m, opm, opmvector, bscan, bscantemp, bscantransposed;
+	Mat bscanl, bscantempl, bscantransposedl;
 	Scalar meanval;
 	Mat lambdas, k, klinear;
+	Mat diffk, slopes, fractionalk, nearestkindex;
+	
 	double lambdamin, lambdamax, kmin, kmax;
 	double pi = 3.141592653589793;
 	
@@ -187,24 +201,81 @@ int main(int argc,char *argv[])
 	// 830 nm to 870 nm,
 	lambdamin = 830e-9;
 	lambdamax = 870e-9;
-	kmin = 2*pi/lambdamax;
-	kmax = 2*pi/lambdamin;
-	double deltalambda = (lambdamax - lambdamin ) / data_y.cols;
-	double deltak = (kmax - kmin) / data_y.cols;
 	
-	// lambdas = linspace(830e-9, 870e-9, data_y.cols)
-	// k = 2*pi/lambdas
-	// klinear = linspace(kmin, kmax, data_y.cols)
-	lambdas = Mat::zeros(cv::Size(1, data_y.cols), CV_64F);
-	klinear = Mat::zeros(cv::Size(1, data_y.cols), CV_64F);
+	double deltalambda = (lambdamax - lambdamin ) / data_y.cols;
+	
+	
+	lambdas = Mat::zeros(cv::Size(1, data_y.cols), CV_64F);		//Size(cols,rows)
+	klinear = Mat::zeros(cv::Size(1, numfftpoints), CV_64F);
+	diffk = Mat::zeros(cv::Size(1, data_y.cols), CV_64F);
+	fractionalk = Mat::zeros(cv::Size(1, numfftpoints), CV_64F);
+	slopes = Mat::zeros(cv::Size(data_y.rows, data_y.cols), CV_64F);
+	nearestkindex = Mat::zeros(cv::Size( 1, numfftpoints ), CV_32S);
+	
+	resizeWindow("Bscan", oph*2, numfftpoints);		// (width,height)
 	
 	for (indextemp=0; indextemp<(data_y.cols); indextemp++) 
 	{
+		// lambdas = linspace(830e-9, 870e-9, data_y.cols)
 		lambdas.at<double>(0,indextemp) = lambdamin + indextemp*deltalambda;
-		k = 2*pi / lambdas;
-		klinear.at<double>(0,indextemp) = kmin + indextemp*deltak;
-		
+	
 	}
+	k = 2*pi / lambdas;
+	kmin = 2*pi/(lambdamax-deltalambda);
+	kmax = 2*pi/lambdamin;
+	double deltak = (kmax - kmin) / numfftpoints;
+	
+	for (indextemp=0; indextemp<(numfftpoints); indextemp++) 
+	{
+		// klinear = linspace(kmin, kmax, numfftpoints)
+		klinear.at<double>(0,indextemp) = kmin + (indextemp+1)*deltak;
+	}
+	
+	
+	
+	//for (indextemp=0; indextemp<(data_y.cols); indextemp++) 
+	//{
+		//printf("k=%f, klin=%f\n", k.at<double>(0,indextemp), klinear.at<double>(0,indextemp));
+	//}
+	
+	
+	for (indextemp=1; indextemp<(data_y.cols); indextemp++) 
+	{
+		//find the diff of the non-linear ks
+		// since this is a decreasing series, RHS is (i-1) - (i)
+		diffk.at<double>(0,indextemp) = k.at<double>(0,indextemp-1) - k.at<double>(0,indextemp);
+		//printf("i=%d, diffk=%f \n", indextemp, diffk.at<double>(0,indextemp));
+	}
+	// and initializing the first point separately
+	diffk.at<double>(0,0) = diffk.at<double>(0,1);
+	
+	for (int f=0; f < numfftpoints; f++)
+	{
+		// find the index of the nearest k value, less than the linear k
+		for (indextemp = 0; indextemp < data_y.cols; indextemp++)
+		{
+			//printf("Before if k=%f,klin=%f \n",k.at<double>(0,indextemp),klinear.at<double>(0,f));
+			if (k.at<double>(0,indextemp) < klinear.at<double>(0,f) ) 
+			{
+				nearestkindex.at<int>(0,f) = indextemp;
+				//printf("After if k=%f,klin=%f,nearestkindex=%d\n",k.at<double>(0,indextemp),klinear.at<double>(0,f),nearestkindex.at<int>(0,f));
+				break;
+				
+			}	// end if
+			
+			
+		}		//end indextemp loop
+		
+	}		// end f loop
+	
+	for (int f=0; f < numfftpoints; f++)
+	{
+				// now find the fractional amount by which the linearized k value is greater than the next lowest k
+		fractionalk.at<double>(0,f) = (klinear.at<double>(0,f) - k.at<double>(0,nearestkindex.at<int>(0,f)) ) / diffk.at<double>(0,nearestkindex.at<int>(0,f));
+		//printf("f=%d, klinear=%f, diffk=%f, k=%f, nearesti=%d\n",f, klinear.at<double>(0,f), diffk.at<double>(0,nearestkindex.at<int>(0,f)), k.at<double>(0,nearestkindex.at<int>(0,f)),nearestkindex.at<int>(0,f) );
+		//printf("f=%d, fractionalk=%f\n",f, fractionalk.at<double>(0,f));
+	}
+
 	
 	
 	timenow = localtime(&now);
@@ -424,10 +495,10 @@ int main(int argc,char *argv[])
         
         indexi = 0;
         indextemp = 0;
-        bscantransposed = Mat::zeros(Size(opw/2, oph), CV_64F);
+        bscantransposed = Mat::zeros(Size(numfftpoints/2, oph), CV_64F);
+	    bscantransposedl = Mat::zeros(Size(opw/2, oph), CV_64F);
 	    
-	    
-        while(1)
+        while(1)		//camera frames acquisition loop
         { 
             ret = GetQHYCCDLiveFrame(camhandle,&w,&h,&bpp,&channels,m.data);
             
@@ -440,15 +511,17 @@ int main(int argc,char *argv[])
 											// because we actually want the columns and not rows
 											// using DFT_ROWS
 											// But that has rolling shutter issues, so going back to rows
-											
-			if ( indexi == 0)
-			{
-				// if it is the first frame acquired, initialize data_yb
-				data_y.copyTo(data_yb);
-			}
-            
-            
-            fps++;
+					
+			if (bkeypressed==1)	
+                 
+					{
+						
+					 data_y.copyTo(data_yb);		// saves the "background" or source spectrum	
+					 bkeypressed=0; 
+						
+					}	
+			
+			fps++;
             t_end = time(NULL);
                 if(t_end - t_start >= 5)
                 {
@@ -474,16 +547,54 @@ int main(int argc,char *argv[])
                 data_y = data_y - meanval(0);
                 
                 // interpolate to linear k space
-	
+                for (int p=0; p<(data_y.rows); p++)
+                {
+					for (int q=1; q<(data_y.cols); q++)
+					{
+						//find the slope of the data_y at each of the non-linear ks
+						slopes.at<double>(p,q) = data_y.at<double>(p,q) - data_y.at<double>(p,q-1);	
+						// in the .at notation, it is <double>(y,x)
+						//printf("slopes(%d,%d)=%f \n",p,q,slopes.at<double>(p,q));
+					}
+					// initialize the first slope separately
+					slopes.at<double>(p,0) = slopes.at<double>(p,1);
+					
+					
+					for (int q=1; q<(data_ylin.cols - 1); q++)
+					{
+						//find the value of the data_ylin at each of the klinear points
+						// data_ylin = data_y(nearestkindex) + fractionalk(nearestkindex)*slopes(nearestkindex)
+						//std::cout << "q=" << q << " nearestk=" << nearestkindex.at<int>(0,q) << std::endl;
+						data_ylin.at<double>(p,q) = data_y.at<double>(p, nearestkindex.at<int>(0,q) ) 
+							 + fractionalk.at<double>(nearestkindex.at<int>(0,q)) 
+								* slopes.at<double>(p,nearestkindex.at<int>(0,q) );	
+						//printf("data_ylin(%d,%d)=%f \n",p,q,data_ylin.at<double>(p,q));
+					}
+					data_ylin.at<double>(p,0) = 0;
+					data_ylin.at<double>(p,numfftpoints) = 0;
+					
+				}
+
                 // InvFFT
                 
-                nr = getOptimalDFTSize( data_y.rows );	//128 when taking transpose(opm, data_y);
-                nc = getOptimalDFTSize( data_y.cols );	//96
+                //std::cout << "data_y.rows = " << data_y.rows << std::endl;
+                //std::cout << "data_ylin.rows = " << data_ylin.rows << std::endl;
+                //std::cout << "slopes.rows = " << slopes.rows << std::endl;
+                //std::cout << "nearestkindex.rows = " << nearestkindex.rows << std::endl;
+                //std::cout << "nearestkindex.cols = " << nearestkindex.cols << std::endl;
+                //std::cout << "klinear.rows = " << klinear.rows << std::endl;
+                //std::cout << "k.rows = " << k.rows << std::endl;
+                
+                nr = getOptimalDFTSize( data_ylin.rows );	//128 when taking transpose(opm, data_y);
+                nc = getOptimalDFTSize( data_ylin.cols );	//96
                 //nc = nc * 4;		// 4x oversampling
+               
                  
-                copyMakeBorder(data_y, padded, 0, nr - data_y.rows, 0, nc - data_y.cols, BORDER_CONSTANT, Scalar::all(0));
+                //copyMakeBorder(data_ylin, padded, 0, nr - data_ylin.rows, 0, nc - data_ylin.cols, BORDER_CONSTANT, Scalar::all(0));
+                normalize(data_ylin, paddedn, 0, 1, NORM_MINMAX);
+                imshow("linearized", paddedn);
 
-				Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+				Mat planes[] = {Mat_<float>(data_ylin), Mat::zeros(data_ylin.size(), CV_32F)};
 				Mat complexI;
 				merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
 
@@ -518,9 +629,54 @@ int main(int argc,char *argv[])
 					
 					imshow( "Bscan", cmagI );
 					
-					bscantransposed = Mat::zeros(Size(opw/2, oph), CV_64F);
+					bscantransposed = Mat::zeros(Size(numfftpoints/2, oph), CV_64F);
 				}
 				 
+				//////////////////////////////////
+				nr = getOptimalDFTSize( data_y.rows );	//128 when taking transpose(opm, data_y);
+                nc = getOptimalDFTSize( data_y.cols );	//96
+                //nc = nc * 4;		// 4x oversampling
+               
+                 
+                copyMakeBorder(data_y, padded, 0, nr - data_y.rows, 0, nc - data_y.cols, BORDER_CONSTANT, Scalar::all(0));
+
+				Mat planesl[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+				Mat complexIl;
+				merge(planesl, 2, complexIl);         // Add to the expanded another plane with zeros
+
+				dft(complexIl, complexIl, DFT_ROWS|DFT_INVERSE);            // this way the result may fit in the source matrix
+
+				// compute the magnitude and switch to logarithmic scale
+				// => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
+				split(complexIl, planesl);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+				magnitude(planesl[0], planesl[1], planesl[0]);// planes[0] = magnitude
+				Mat magIl = planesl[0];
+				Mat cmagIl;
+
+				magIl += Scalar::all(1);                    // switch to logarithmic scale
+				log(magIl, magIl);
+				if(indextempl < averages)
+				{
+					bscantempl = magIl.colRange(0,nc/2);
+					bscantempl.convertTo(bscantempl,CV_64F);
+					accumulate(bscantempl, bscantransposedl);
+					indextempl++;
+				}
+				else
+				{
+					indextempl = 0;
+					transpose(bscantransposedl, bscanl); 
+					// remove dc
+					bscanl.row(0).setTo(Scalar(0));
+				
+					normalize(bscanl, bscanl, 0, 1, NORM_MINMAX);
+					bscanl.convertTo(bscanl, CV_8UC1, 255.0);
+					applyColorMap(bscanl, cmagIl, COLORMAP_JET);
+					
+					imshow( "Bscanl", cmagIl );
+					
+					bscantransposedl = Mat::zeros(Size(opw/2, oph), CV_64F);
+				}
 				 
 				
 				
@@ -546,20 +702,7 @@ int main(int argc,char *argv[])
 					skeypressed=0; 	 
 						
 					}
-				 
-									
-				if (bkeypressed==1)	
-                 
-					{
-						
-					 data_y.copyTo(data_yb);		// saves the "background" or source spectrum	
-					 bkeypressed=0; 
-						
-					}	
-					 
-			
-
-					 
+				
 					 
                 key=waitKey(30); // wait 30 milliseconds for keypress
                 // max frame rate at 1280x960 is 30 fps => 33 milliseconds
